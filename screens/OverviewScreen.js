2095bf70a6d966601cc7b735790d4f69d81afcb9
Erick Perez <erickeperez24@gmail.com>
2021-01-04 20:10:21 -0500

    Added style to styleshee

--- screens/OverviewScreen.js
+++ screens/OverviewScreen.js
@@ -6,7 +6,12 @@ const OverviewScreen = props => {
 };
 
 const style = StyleSheet.create({
-
+    container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
 
 export default OverviewScreen;
